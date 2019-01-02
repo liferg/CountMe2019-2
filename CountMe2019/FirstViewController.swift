15dc3cf2052d0446898301f2621d940021d5c780
SIEVWRIGHT, CORINN <776515@ehs-s-232-18.local>
2019-01-02 10:41:22 -0600

    Initial Commit

---
+++ CountMe2019/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CountMe2019
+//
+//  Created by SIEVWRIGHT, CORINN on 1/2/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CountMe2019/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CountMe2019
+//
+//  Created by SIEVWRIGHT, CORINN on 1/2/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
