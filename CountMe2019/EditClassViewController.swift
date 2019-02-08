f195c90544785eb4ec01441b7c2ac17262e401b6
Uma Parhar <umaparhar@umas-macbook-air.local>
2019-02-07 22:06:30 -0600

    class picker working

--- CountMe2019/EditClassViewController.swift
+++ CountMe2019/EditClassViewController.swift
@@ -8,61 +8,63 @@
 
 import UIKit
 
-class EditClassViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
+class EditClassViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource
+{
+    var classP: Int = 0
+    var pickerData: [Classroom] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
+        self.ClassPicker.delegate = self
+        self.ClassPicker.dataSource = self
+        pickerData = classesArray
         /*if(classesArray.count == 0)
         {
             performSegue(withIdentifier: "toEdit", sender: self)
         }
-        
-        var uma = Student("Uma", "Parhar")
-        studentsArray.append(uma)
- */
-        /*
-        var history = Classroom(1, "history")
-        classesArray.append(history)
-        
-        var science = Classroom(2, "science")
-        classesArray.append(science)
-         */
+        */
         
 
         // Do any additional setup after loading the view.
+
     }
-    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+       classP = row
+    }
+    @IBOutlet weak var ClassPicker: UIPickerView!
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       return classesArray[0].classList.count
-        /*if(classesArray.count != 0)
+        if(classesArray.count != 0)
         {
             return classesArray[0].classList.count
         }
         else
         {
-            performSegue(withIdentifier: "toEdit", sender: self)
             return 0
         }
+        /*performSegue(withIdentifier: "toEdit", sender: self)
  */
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell" , for: indexPath) as! CollectionViewCell
-        cell.myLabel.text = classesArray[0].classList[indexPath.row].firstName + " " + classesArray[0].classList[indexPath.row].lastName
+        cell.myLabel.text = classesArray[classP].classList[indexPath.row].firstName + " " + classesArray[classP].classList[indexPath.row].lastName
         cell.index = indexPath.row
-        /*for i in stride(from: 0, to: classesArray.count, by: 1)
-        {
-        cell.myLabel.text = classesArray[i].className
-        }
- */
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(indexPath.item)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return classesArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String
+{
+    return pickerData[row].className
     }
     /*
     // MARK: - Navigation
