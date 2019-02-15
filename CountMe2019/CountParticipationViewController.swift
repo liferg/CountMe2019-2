4cb9174456bbebc540820b8df60e65cec3680bf5
Lauren <lauren@ehs-s-xlib-112.local>
2019-02-15 11:05:18 -0600

    Swipe delete student on edit class working

--- CountMe2019/CountParticipationViewController.swift
+++ CountMe2019/CountParticipationViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class CountParticipationViewController: UIViewController {
-
+class CountParticipationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return and sets number of rows
+        var x = classesArray[0].classList.count
+        return x
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        var name = classesArray[0].classList[indexPath.row]
+        cell.textLabel?.text = name.firstName + " " + name.lastName
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let delete = UITableViewRowAction(style: .destructive, title: "delete") { (action, indexPath) in
+            classesArray[0].classList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        return [delete]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
+
+
 
