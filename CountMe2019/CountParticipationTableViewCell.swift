db7edf5e3631ce98ffc82f8c46d9ff569c488434
Corinn <csievwright12@gmail.com>
2019-04-08 10:31:19 -0500

    table view cell added

---
+++ CountMe2019/CountParticipationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountParticipationTableViewCell.swift
+//  CountMe2019
+//
+//  Created by Corinn on 4/8/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class CountParticipationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
